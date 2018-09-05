f8b1362e1c68d5b162b81cd95c20f47702a7b25a
Ben Fowlie <bsfowlie@gmail.com>
2018-09-04 23:42:54 -0400

    Initial project

---
+++ src/test/java/com/github/bsfowlie/textadventure/MainTest.java
@@ -0,0 +1,21 @@
+package com.github.bsfowlie.textadventure;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.assertj.core.api.WithAssertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple Application.
+ */
+final class MainTest implements WithAssertions
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertThat( true ).isTrue();
+    }
+}
