88381e01cb4564dfb9af09b2151fb73bea65b654
Ben Fowlie <bsfowlie@gmail.com>
2018-09-06 22:34:39 -0400

    Game knows if it is running or not

--- src/main/java/com/github/bsfowlie/textadventure/Game.java
+++ src/main/java/com/github/bsfowlie/textadventure/Game.java
@@ -2,7 +2,20 @@ package com.github.bsfowlie.textadventure;
 
 public class Game {
 
+    private final Room currentRoom;
+
+    private final EndCondition ending;
+
     public Game(final Room startingRoom, final EndCondition endCondition) {
+
+        currentRoom = startingRoom;
+        ending = endCondition;
+
+    }
+
+    public boolean isRunning() {
+
+        return !ending.isEndReached();
 
     }
 
